307bd6ad6072c96d6ca807cb15bcbc82df6b39af
vianademelovictor-cloud <vianademelovictor@gmail.com>
2026-08-12 14:17:13 -0300

    Corrige sintaxe da string de conexao

--- app_web.py
+++ app_web.py
@@ -126,8 +126,7 @@ st.markdown(
 # --- 3. CAMADA DE DADOS (SUPABASE POSTGRESQL) ---
 def conectar():
     # Substitua [SUA_SENHA_AQUI] pela senha que você gerou no Supabase!
-    return psycopg2.connect("postgresql://postgres.eqynneburaxsgfqyjjcd:tnAmjlBG7bwR2nUz
-@aws-0-sa-east-1.pooler.supabase.com:6543/postgres")
+    return psycopg2.connect("postgresql://postgres.eqynneburaxsgfqyjjcd:tnAmjlBG7bwR2nUz@aws-0-sa-east-1.pooler.supabase.com:6543/postgres")
 
 def inicializar_banco():
     conn = conectar()
